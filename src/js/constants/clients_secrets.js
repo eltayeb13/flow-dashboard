499abdf80fe1435e544215f0363f2b16b0b4a261
eltayeb13 <101056798+eltayeb13@users.noreply.github.com>
2022-03-29 20:03:50 +0300

    Create clients_secrets.js

---
+++ src/js/constants/clients_secrets.js
@@ -0,0 +1,12 @@
+var client_secrets = {
+
+	// Match with settings/secrets.py and GCP console
+	G_OAUTH_CLIENT_ID: "######.XXXXXXXXXXXX.apps.googleusercontent.com",
+	DEV_G_OAUTH_CLIENT_ID: "######.XXXXXXXXXXXX.apps.googleusercontent.com",
+	GOOGLE_API_KEY: "XXXXXXXXXXXX",
+
+	// Goodreads
+	GR_API_KEY: ""
+};
+
+module.exports = client_secrets;
